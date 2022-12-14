8a0f28dd0a295933997e626e1f63bb0d31fb4615
Donguk Kim <ehddnr73@naver.com>
2022-12-15 05:40:17 +0900

    Cocoa MVC

---
+++ iOSArchitecturePatterns.swiftpm/MVC/CocoaMVC.swift
@@ -0,0 +1,39 @@
+//
+//  CocoaMVC.swift
+//  
+//
+//  Created by 김동욱 on 2022/12/15.
+//
+
+import UIKit
+import Foundation
+
+/// Model object
+struct Counter {
+    var number: Int
+}
+
+/// View + Controller object
+class CounterViewController: UIViewController {
+    var counter: Counter!
+    // UI Component
+    let numberLabel = UILabel()
+    let increaseNumberButton = UIButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        increaseNumberButton.addTarget(self, action: #selector(touchUpButton), for: .touchUpInside)
+        // set up a layout & configure
+    }
+    
+    @objc func touchUpButton() {
+        counter.number += 1
+        numberLabel.text = String(counter.number)
+    }
+}
+
+let model = Counter(number: 0)
+let view = CounterViewController()
+view.counter = model
